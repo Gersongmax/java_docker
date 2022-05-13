632195b469cd8a0df9fa385d193fee6fcf05e485
Gersongmax <gersongmax@hotmail.com>
2022-05-13 15:59:14 -0300

    ola_unicamp

---
+++ src/ola_unicamp/Ola_unicamp.java
@@ -0,0 +1,12 @@
+package ola_unicamp;
+
+public class Ola_unicamp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola Unicamp!!!");
+		
+
+	}
+
+}
---
+++ src/ola_unicamp/string.java
@@ -0,0 +1,8 @@
+package ola_unicamp;
+
+public class string {
+	public static void main(string[] args) {
+		System.out.println("Ola Unicamp");
+		}
+
+}
